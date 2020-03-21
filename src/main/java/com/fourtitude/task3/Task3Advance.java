d46a54e7e2b847c357e89ad50968fcf98d1efdbf
WeiYang94 <cwyang1994@gmail.com>
2020-03-21 14:50:53 +0800

    Testing

--- src/main/java/com/fourtitude/task3/Task3Advance.java
+++ src/main/java/com/fourtitude/task3/Task3Advance.java
@@ -1,11 +1,14 @@
 package com.fourtitude.task3;
 
+import java.util.Base64;
+
 public class Task3Advance {
 	public static void main(String[] args) {
 		String input = "haha";
 		try {
 			String encrypt = encrypt(input);
-			System.out.println(encrypt);
+			
+			decrypt(encrypt);
 		} catch (Exception e) {
 			System.out.println("Error during encryption/decryption.");
 		}
@@ -20,11 +23,32 @@ public class Task3Advance {
 		for (int ix = 0; ix < len; ix += 1) {
 			xors[ix] = (byte) ((int) bytes[ix] ^ ((int) lcg.next()));
 		}
-		return new String(java.util.Base64.getEncoder().encode(xors), "UTF-8");
+		
+		String encry = new String(java.util.Base64.getEncoder().encode(xors), "UTF-8");
+		
+		byte[] aa = Base64.getEncoder().encode(xors);
+		System.out.println(java.util.Base64.getEncoder().encode(xors));
+		System.out.println(encry.getBytes("UTF-8"));
+		
+		return encry;
+//		return new String(java.util.Base64.getEncoder().encode(xors), "UTF-8");
 	}
 	
 	public static String decrypt(String base64EncodedValue) throws Exception {
-	    throw new UnsupportedOperationException("TODO: to be implemented");
+		Lcg lcg = new Lcg(256, 11, 1, 0);
+		byte[] decodedByte = Base64.getDecoder().decode(base64EncodedValue.getBytes("UTF-8"));
+//		System.out.println(decodedByte);
+//		System.out.println(base64EncodedValue.getBytes("UTF-8"));
+		int len = decodedByte.length;
+		byte[] newXors = new byte[len];
+		for (int ix = 0; ix < len; ix += 1) {
+			newXors[ix] = (byte) ((int) decodedByte[ix] ^ ((int) lcg.next()));
+		}
+		
+		return (new String(newXors, "UTF-8"));
+		
+		
+//	    throw new UnsupportedOperationException("TODO: to be implemented");
 	}
 
 
